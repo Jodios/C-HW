bdce2df4f1fee706bbefa8491ac50216c8ed6c04
sirsamhain <joelandapophis@gmail.com>
2018-04-08 15:28:01 -0500

    Finshed Problem 0 :)

---
+++ HWCH11/P1/P1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Fraction{
+  int numer;
+  int denom;
+  static int counter;
+public:
+  Fraction(int n = 0, int d = 1):numer(n), denom(d){counter++; print();};
+  Fraction(const Fraction &fRef){
+    numer = fRef.numer;
+    denom = fRef.denom;
+    counter++;
+    std::cout << "--------" << std::endl;
+    std::cout << "|copied|" << std::endl;
+    std::cout << "--------" << std::endl;
+    print();
+  }
+  int getNumer(){return numer;}
+  int getDenom(){return denom;}
+  void setNumer(int numer){this->numer = numer;}
+  void setDenom(int denom){this->denom = denom;}
+  void print(){
+    std::cout << "Objects in existence: " << counter << std::endl;
+    std::cout << "Numer: " << numer << "\nDenom: " << denom << std::endl; 
+  }
+
+};
+
+int Fraction::counter = 0;
+
+int main(){
+
+  Fraction f1, f2 = f1;
+
+  return 0;
+}
+
